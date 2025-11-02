1a03aec19805fb30353fb6de9f8f36d6d8a9738a
Richard_Amoah <amoahrichdad@gmail.com>
2025-11-02 22:10:06 +0000

    Fix formatting and import statements in admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,6 @@
 from django.db import models
 from .models import Book
-
+"register", "admin.ModelAdmin"
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -8,4 +8,5 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
 
