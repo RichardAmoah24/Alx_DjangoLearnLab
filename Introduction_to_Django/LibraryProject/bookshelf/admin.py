5ba48f9cf775106e2163ba6dc0eaf1324e2c1ed3
Richard_Amoah <amoahrichdad@gmail.com>
2025-11-02 22:12:04 +0000

    Enhance Book model admin configuration
    Added list display, search fields, and filters to the Book model in the admin interface.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,11 +2,12 @@ from django.db import models
 from .models import Book
 "register", "admin.ModelAdmin"
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_year = models.PositiveIntegerField()
+   list_display = ("title", "author", "publication_year")
+    search_fields = ("title", "author")
+    list_filter = ("publication_year",)
 
     def __str__(self):
         return self.title
+
 
 
