defc7d8e637e37f193bb28f9a909814f4dc694d2
Richard_Amoah <amoahrichdad@gmail.com>
2025-11-02 22:03:34 +0000

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
