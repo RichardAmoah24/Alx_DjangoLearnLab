55e49f2e605ccb437927a7ed902001b8185d6b45
Richard_Amoah <amoahrichdad@gmail.com>
2025-11-02 22:05:31 +0000

    Refactor admin.py to import Book model

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .models import Book
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -7,3 +8,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
